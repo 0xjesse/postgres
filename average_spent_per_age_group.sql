ef6a8817a9913be458cce1a43d48af58ceba9007
0xjesse <100794303+0xjesse@users.noreply.github.com>
2023-09-08 10:45:24 -0500

    Create average_spent_per_age_group.sql
    Breakdown of the average spent per age group

---
+++ average_spent_per_age_group.sql
@@ -0,0 +1,17 @@
+SELECT
+    CASE
+        WHEN age < 18 THEN 'Under 18'
+        WHEN age BETWEEN 18 AND 24 THEN '18-24'
+        WHEN age BETWEEN 25 AND 34 THEN '25-34'
+        WHEN age BETWEEN 35 AND 44 THEN '35-44'
+        WHEN age BETWEEN 45 AND 54 THEN '45-54'
+        WHEN age BETWEEN 55 AND 64 THEN '55-64'
+        ELSE '65 and Over'
+    END AS age_group,
+    AVG(total_amount) AS average_amount
+FROM
+    sales
+GROUP BY
+    age_group
+ORDER BY
+    age_group;
